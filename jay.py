15996a79561c80df444a21978123bf4a03dea13f
vajib-max <65997884+vajib-max@users.noreply.github.com>
2024-08-29 20:52:42 +0530

    uploaded
    uploaded

---
+++ jay.py
@@ -0,0 +1,27 @@
+a=int(input('number of players :'))
+player=[]
+for i in range(0,a,1):
+    B1=(input('player name :'))
+    player.append(B1)
+print(player)
+
+jaytotal=[]
+abitotal=[]
+vajitotal=[]
+sanjaytotal=[]
+for j in range(1,8,1):
+    print('---------ROUND',i,'----------')
+    for k in range(0,a,1):
+        p1=int(input(player[k]))
+        jaytotal += p1
+    print(jaytotal)
+
+
+
+
+
+
+
+
+
+
